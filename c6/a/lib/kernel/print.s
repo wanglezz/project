3a706f7b2b84cc0cb80adb19dd4bcb02b6023252
xiwangderen <2263097228@qq.com>
2022-11-08 20:01:06 -0800

    MOD c6

--- c6/a/lib/kernel/print.s
+++ c6/a/lib/kernel/print.s
@@ -4,7 +4,9 @@ SELECTOR_VIDEO  equ     (0x0003<<3) + TI_GDT + RPL0
 
 [bits 32]
 section .text
-
+;--------------------------------------- put_char --------------------------------------------
+;function:Writes a character from the stack to the cursor position
+;---------------------------------------------------------------------------------------------
 global put_char
 put_char:
         pushad
@@ -90,4 +92,120 @@ put_char:
 	loop .cls
 	mov bx, 1920	;The cursor points to the beginning of the last line
 
-		
+.set_cursor:
+;----------the higher eight-----------
+	mov dx, 0x03d4
+	mov al, 0x0e
+	out dx, al
+	mov dx, 0x03d5
+	mov al, bh
+	out dx, al
+
+;----------the lower eight---------
+	mov dx, 0x03d4
+        mov al, 0x0f
+        out dx, al
+        mov dx, 0x03d5
+        mov al, bl
+        out dx, al
+
+.put_char_done:
+	popad
+	ret
+	
+
+[bits 32]
+section .text
+;--------------------------------------- put_str --------------------------------------------
+;function:put_str prints a string ending in 0 characters by put_char
+;---------------------------------------------------------------------------------------------
+
+
+global put_str
+put_str:
+	push ebx	
+	push ecx	;put_str only need ebx and ecx
+	xor ecx, ecx
+	mov ebx, [esp + 12]	;ebx save the address of string to be printed
+
+.goon:
+	mov cl, [ebx]
+	cmp cl, 0
+	jz .str_over
+	
+	push ecx	;Pass the parameters for put_char
+	call put_char
+	add esp, 4	
+	inc ebx
+	jmp .goon
+
+.str_over:
+	pop ecx
+	pop ebx
+	ret
+
+
+section .data
+put_int_buffer dq 0
+;--------------------------------------- put_int --------------------------------------------
+;function:Implement integer printing
+;---------------------------------------------------------------------------------------------
+
+global put_int
+put_int:
+	pushad
+	mov ebp, esp
+	mov eax, [ebp + 4*9]
+	mov edx, eax
+	mov edi, 7
+	mov ecx, 8
+	mov ebx, put_int_buffer
+
+.16based_4bits:
+	and edx, 0x0000000F
+	
+	cmp edx, 9
+	jg .is_A2F
+	add edx, '0'
+	jmp .store
+
+.is_A2F:
+	sub edx, 10
+	
+	add edx, 'A'
+
+.store:
+	mov [ebx + edi]	, dl
+	dec edi
+	shr eax, 4
+	mov edx, eax
+	loop .16based_4bits
+
+.ready_to_print:
+	inc edi	;before edi is 0xffffffff,inc makes it 0
+
+.skip_prefix_0:
+	cmp edi, 8	;if edi = 8,that means the number is 0
+	je .full0
+	
+.go_on_skip:
+	mov cl, [put_int_buffer + edi]
+	inc edi
+	cmp cl, '0'
+	je .skip_prefix_0
+	dec edi
+	jmp .put_each_num
+
+.full0:
+	mov cl, '0'
+	
+.put_each_num:
+	push ecx	;Pass the parameters for put_char
+	call put_char
+	add esp, 4
+	inc edi
+	mov cl, [put_int_buffer + edi]
+	cmp edi, 8
+	jl .put_each_num
+	popad
+	ret
